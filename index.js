a0623b34bef1ac20d1cf57906ba5bf3e91c5c3ef
Your Nameimane <git config --global user.email you@example.com>
2024-11-21 12:42:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let name = "hello world"
+console.log(typeof(name));
+
+let x = 10
+x = 20
+console.log(x);
+ let fruit = {
+    apple:"green",
+    banana:"yellow",
+    
+
+
+ }
+ console.log(fruit.apple)
+
+ let password = "letmein";
+ if ( password==="letmein"){
+    console.log(true);
+
+
+ }else{
+    console.log(false);
+ }
+
+ for(let i=1 ; i <=30; i++){
+    if (i===%2;
+    console.log(i);
+
+ }
+ 
