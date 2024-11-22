e064a93a22703d907ab0160317d02b00295b2392
Your Nameimane <git config --global user.email you@example.com>
2024-11-22 17:03:45 +0100

    good

--- test.js/ex.js
+++
@@ -1,9 +0,0 @@
-for (let i=1; i<=30; i++){
-if (i %2==1 ){
-    console.log(i)
-}
-}
-const fruits=["pomme", "banana", "cerise",];
-for(k=0;k<fruits.length;k++){
-console.log(fruits[k].toUpperCase())
-}
